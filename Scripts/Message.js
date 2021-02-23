ae466b90fb54db5e940c61baf45c505e75f375c7
apoca <tyler.burke1@msubillings.edu>
2021-02-22 20:04:35 -0700

    Added Messaging functionality

--- Scripts/Message.js
+++ Scripts/Message.js
@@ -1,48 +1,47 @@
-var message = createBox("Hello World", "12:22", "../placeholder/images/treti.png","Treti")
-var messageTime = getMessageTime();
-var userImage = "../placeholder/images/treti.png"
-var userName = "Treti"
+"use strict";
+var userImage = "../placeholder/images/treti.png";
+var userName = "Treti";
+var messageList = document.getElementById("messageList");
+var chatWindow = document.getElementById("chatWindow");
 
-var messageContent = document.getElementById(messageInput).value;
-
-document.querySelector(messageContent).addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        $(".messageList").append(message)
-    }
-});
-
-
-function createMessage(messageContent, messageTime, userImage, userName){
-    var html = "";
-    html += 
-    '<div class="flex mx-5 my-3 py-4 border-5 border-gray-700">' +
-                '<div class="flex-none">' +
-                  '<a href="#"><img src="' + userImage +'"alt="'+userName+"_img" +'" class="w-10 h-10 rounded-xl"></img></a>' +
-                '</div>' +
-                '<div class="ml-5">' +
-                  '<div>' +
-                    '<a href="#" class="text-white hover:underline">'+userName+'</a>' +
-                    '<span class="text-xs text-gray-600 ml-1">'+messageTime+'</span>' +
-                  '</div>' +
-                  '<div>' +
-                  '<div>'+ messageContent +'</div>' +
-                '</div>' +
-              '</div>' +
-            '</div>' +
-          '</div>'
+function createMessage(messageInput, messageTime, userImage, userName) {
+    var html= `<div class="flex mx-5 my-3 py-4 border-5 border-gray-700"><div class="flex-none">
+            <a href="#"><img src=" ${userImage}"alt=" ${userName}_img" class="w-10 h-10 rounded-xl"></img></a>
+            </div><div class="ml-5"><div>
+            <a href="#" class="text-white hover:underline">${userName}</a>
+            <span class="text-xs text-gray-600 ml-1">${messageTime} </span></div>
+            <div><div>${messageInput} </div></div></div></div></div>`;
     return html;
 }
-function getMessageTime(){
-    var tempTime = new Date()
+function getMessageTime() {
+    var tempTime = new Date();
     var AMPM;
-    if(tempTime.getHours() > 12){
-        AMPM = "PM"
+    if (tempTime.getHours() > 12) {
+        AMPM = "PM";
+        var tempTimeHours = tempTime.getHours() - 12;
     }
-    else{
-        AMPM = "AM"
+    else {
+        AMPM = "AM";
+        var tempTimeHours = tempTime.getHours();
     }
-    var messageTime = tempTime.getHours() + ":" + tempTime.getMinutes() + AMPM
+    var messageTime = `Today at ${tempTimeHours}:${tempTime.getMinutes()} ${AMPM}`;
     return messageTime;
+}
+function update() {
+    var messageInput = document.forms['MessageForm']['messageInput'].value = "";
+    //This line is supposed to automatically keep the page scrolling to the most recent message
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+    return messageInput;
+}
+function sendMessage() {
+    var messageInput = document.forms['MessageForm']['messageInput'].value
+    var messageTime = getMessageTime();
+    var message = createMessage(messageInput, messageTime, userImage, userName);
+    var messageSlot = document.createElement('li');
+    messageSlot.innerHTML = message;
+    messageList.appendChild(messageSlot);
+    update();
+    return false;
 }
 
 
