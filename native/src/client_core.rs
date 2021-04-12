7f8e963721cd1670e7532357244f3252e678be1a
klinical <zach.n500@gmail.com>
2021-04-12 12:53:56 -0500

    Refactor + start of NetBuffer type and impl

---
+++ native/src/client_core.rs
@@ -0,0 +1,201 @@
+use std::{io::{Read, Write}, net::TcpStream, str::from_utf8};
+use std::{error::Error, fmt};
+
+#[derive(Debug)]
+pub struct Message {
+    pub source: String,
+    pub content: String
+}
+
+impl PartialEq for Message {
+  fn eq(&self, other: &Self) -> bool {
+    (self.source == other.source) && (self.content == other.content)
+  }
+}
+
+#[derive(Debug)]
+struct RablParseError {
+  error: String
+}
+
+impl RablParseError {
+  fn new(msg: &str) -> RablParseError {
+    RablParseError { error: msg.to_string() }
+  }
+}
+
+impl fmt::Display for RablParseError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f,"{}",self.error)
+  }
+}
+
+impl Error for RablParseError {
+  fn description(&self) -> &str {
+    &self.error
+  }
+}
+
+struct NetBuffer { 
+  buffer: [u8; 4096],
+  len: usize
+}
+/*
+impl NetBuffer {
+  fn split_by_unit(&self) -> Vec<String> {
+    let v: Vec<u8> = Vec::new();
+    for (i, byte) in self.buffer.iter().enumerate() {
+      if byte == &31 || byte == &23 {
+
+      } 
+    }
+
+    (str_one, str_two)
+  }
+
+  fn from(&self, payload: &[u8]) {}
+}
+
+// For easy displaying
+// TODO: convert to utf8 first for even easier displaying
+impl<T: Integer + fmt::Debug> fmt::Debug for NetBuffer<T> {
+  fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+    self.buffer[..].fmt(formatter)
+  }
+}
+*/
+// Internal lib function to establish a connection
+fn establish_connection() -> Result<TcpStream, Box<dyn Error>> {
+    match TcpStream::connect("64.227.87.184:5050") {
+        Ok(stream) => {
+            Ok(stream)
+        },
+        Err(e) => Err(Box::new(e))
+    }
+}
+
+// Take in a series of Strings and build a server request using specific sep characters
+pub fn build_payload(components: Vec<String>) -> Vec<u8> {
+    let mut payload: Vec<u8> = vec![];
+    for (i, c) in components.iter().enumerate() {
+        let mut x = c.clone().into_bytes();
+        payload.append(&mut x);
+        if i == components.len() - 1 {
+            // End of payload
+            payload.push(31);
+        } else {
+            // End of payload unit
+            payload.push(29);
+        }
+    }
+    payload
+}
+
+pub fn login(username: String, password: String) -> Result<bool, Box<dyn Error>> {
+  let payload = build_payload(vec!["login".to_string(), username, password]);
+  // Establish connection and write the payload
+  let mut stream = establish_connection().unwrap();
+  if let Err(err) = stream.write(&payload) {
+      panic!("{}", &err.to_string());
+  }
+
+    // 16 byte buffer
+    let mut incoming_data = [0 as u8; 16];
+    let data_size = stream.read(&mut incoming_data).unwrap();
+    let response = from_utf8(&incoming_data[0..data_size]).unwrap();
+
+  if response == "login.grant" {
+      println!("Login successful!");
+      Ok(true)
+  } else {
+      Ok(false)
+  }  
+}
+
+pub fn send_message(sender: String, target: String, message: String) -> Result<(), Box<dyn Error>>{
+  let payload = build_payload(vec!["msg".to_string(), sender, target, message]);
+
+  // Arguments good - begin connection
+  // panic! is going to propogate our errors up to be handled by the JavaScript
+  let mut stream = establish_connection()?;
+  stream.write(&payload)?;
+
+  Ok(())
+}
+
+// If successful, returns Some(Vec of messages)
+pub fn poll_messages(username: String) -> Result<Option<Vec<Message>>, Box<dyn Error>> {
+  let mut stream = establish_connection()?;
+  let payload = build_payload(vec!["poll".to_string(), username]);
+
+  stream.write(&payload)?;
+
+  // Create the buffer, currently 128 bytes
+  // we then 'read' into the buffer, filling it with whatever the server sends us
+  let mut buffer = [0 as u8; 128];
+  stream.read(&mut buffer)?;
+
+  // Now we need to take that buffer and make an vec of messages, assuming the server did not respond with nil
+  // First, check to see if buffer contains 'nil', if so simply return None
+  // Otherwise, we build the vec from the buffer contents
+  match &buffer[0..2] {
+    b"nil" => {
+      Ok(None)
+    },
+    _ => {
+      // Great! Buffer is not nil
+      let mut messages: Vec<Message> = Vec::new(); 
+
+      // Cursor - A sort of index - used to keep track of which part of the buffer we are at to aid in slicing
+      let mut cursor = 0;
+      for (i, c) in buffer.iter().enumerate() {
+
+        // If c is either the end of a source:msg block or end of the returned data
+        if c == &29 || c == &23 {
+          // We will create a new sub-slice from the beginning of our cursor to the current index
+          // This will effectively create a sub slice, of each source:msg block
+          let slice = &buffer[cursor..i].to_owned();
+
+          for (j, x) in slice.iter().enumerate() {
+            // Now we iterate the sub slice, looking for the ascii-31, which we use to seperate source from msg
+            if x == &31 {
+              // Attempt to parse the source and message from bytes to utf-8
+              match (from_utf8(&slice[0..j]), from_utf8(&slice[j+1..slice.len()])) {
+                (Ok(source), Ok(message)) => {
+                  messages.push(Message { source: source.to_string(), content: message.to_string() } )
+                },
+                _ => return Err(Box::new(RablParseError::new("Failed to parse in poll_messages")))
+              }  
+            }
+          }
+
+          // Advance our cursor to the outer-loop index+1 (Moves us to the next source:msg block)
+          cursor = i+1;
+        }
+      }
+
+      Ok(Some(messages))
+    }
+  }
+}
+
+pub fn poll_friends(username: String) -> Result<Vec<String>, Box<dyn Error>> {
+  let mut stream = establish_connection()?;
+  let payload = build_payload(vec!["pollfriends".to_string(), username]);
+
+  stream.write(&payload)?;
+  let mut buffer = [0 as u8; 256];
+  let _data_len = stream.read(&mut buffer)?;
+
+  let mut friends_list: Vec<String> = Vec::new();
+  let mut cursor: usize = 0;
+  for (i, char) in buffer.iter().enumerate() {
+    if char == &31 || char == &23 {
+      let utf_str = from_utf8(&buffer[cursor..i])?;
+      friends_list.push(utf_str.to_owned());
+      cursor = i+1;
+    }
+  } 
+  
+  Ok(friends_list)
+}
--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,9 +1,17 @@
 #![allow(non_snake_case)]
 
 use neon::prelude::*;
-use core::panic;
-use std::{collections::HashMap, io::{Read, Write}, net::TcpStream, str::from_utf8};
-use std::{error::Error, fmt};
+
+mod client_core;
+use crate::client_core::*;
+
+register_module!(mut cx, {
+    cx.export_function("login", login_clicked)?;
+    cx.export_function("send_message", send_message_clicked)?;
+    cx.export_function("poll_messages", poll_messages_clicked)?;
+    cx.export_function("poll_friends", handle_poll_friends)?;
+    Ok(())
+});
 
 fn handle_poll_friends(mut cx: FunctionContext) -> JsResult<JsArray> {
   let username = cx.argument::<JsString>(0)?.value();
@@ -16,66 +24,6 @@ fn handle_poll_friends(mut cx: FunctionContext) -> JsResult<JsArray> {
   }
 
   Ok(JsFriendsList)
-}
-
-fn poll_friends(username: String) -> Result<Vec<String>, Box<dyn Error>> {
-  let mut stream = establish_connection()?;
-  let payload = build_payload(vec!["pollfriends".to_string(), username]);
-
-  stream.write(&payload)?;
-  let mut buffer = [0 as u8; 256];
-  let _data_len = stream.read(&mut buffer)?;
-
-  let mut friends_list: Vec<String> = Vec::new();
-  let mut cursor: usize = 0;
-  for (i, char) in buffer.iter().enumerate() {
-    if char == &31 || char == &23 {
-      let utf_str = from_utf8(&buffer[cursor..i])?;
-      friends_list.push(utf_str.to_owned());
-      cursor = i+1;
-    }
-  } 
-  
-  Ok(friends_list)
-}
-
-// Take in a series of Strings and build a server request using specific sep characters
-fn build_payload(components: Vec<String>) -> Vec<u8> {
-    let mut payload: Vec<u8> = vec![];
-    for (i, c) in components.iter().enumerate() {
-        let mut x = c.clone().into_bytes();
-        payload.append(&mut x);
-        if i == components.len() - 1 {
-            // End of payload
-            payload.push(31);
-        } else {
-            // End of payload unit
-            payload.push(29);
-        }
-    }
-    payload
-}
-
-fn login(username: String, password: String) -> Result<bool, Box<dyn Error>> {
-  let payload = build_payload(vec!["login".to_string(), username, password]);
-  // Establish connection and write the payload
-  let mut stream = establish_connection().unwrap();
-  if let Err(err) = stream.write(&payload) {
-      panic!("{}", &err.to_string());
-  }
-
-    // 16 byte buffer
-    let mut incoming_data = [0 as u8; 16];
-    let data_size = stream.read(&mut incoming_data).unwrap();
-    let response = from_utf8(&incoming_data[0..data_size]).unwrap();
-
-  if response == "login.grant" {
-      println!("Login successful!");
-      Ok(true)
-  } else {
-      Ok(false)
-  }
-  
 }
 
 fn login_clicked(mut cx: FunctionContext) -> JsResult<JsBoolean> {
@@ -93,17 +41,6 @@ fn login_clicked(mut cx: FunctionContext) -> JsResult<JsBoolean> {
     }
 }
 
-fn send_message(sender: String, target: String, message: String) -> Result<(), Box<dyn Error>>{
-  let payload = build_payload(vec!["msg".to_string(), sender, target, message]);
-
-  // Arguments good - begin connection
-  // panic! is going to propogate our errors up to be handled by the JavaScript
-  let mut stream = establish_connection()?;
-  stream.write(&payload)?;
-
-  Ok(())
-}
-
 fn send_message_clicked(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     // Gathering parameters from JavaScript
     let sender = cx.argument::<JsString>(0)?.value();
@@ -111,119 +48,6 @@ fn send_message_clicked(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     let message = cx.argument::<JsString>(2)?.value();
     send_message(sender, target, message).unwrap();
     Ok(cx.undefined())
-}
-
-register_module!(mut cx, {
-    cx.export_function("login", login_clicked)?;
-    cx.export_function("send_message", send_message_clicked)?;
-    cx.export_function("poll_messages", poll_messages_clicked)?;
-    cx.export_function("poll_friends", handle_poll_friends)?;
-    Ok(())
-});
-
-// Internal lib function to establish a connection
-fn establish_connection() -> Result<TcpStream, Box<dyn Error>> {
-    match TcpStream::connect("64.227.87.184:5050") {
-        Ok(stream) => {
-            Ok(stream)
-        },
-        Err(e) => Err(Box::new(e))
-    }
-}
-#[derive(Debug)]
-struct MessageBlob {
-    blob: HashMap<String, Vec<Message>>
-}
-
-#[derive(Debug)]
-struct Message {
-    source: String,
-    content: String
-}
-
-impl PartialEq for Message {
-  fn eq(&self, other: &Self) -> bool {
-    (self.source == other.source) && (self.content == other.content)
-  }
-}
-
-#[derive(Debug)]
-struct RablParseError {
-  error: String
-}
-
-impl RablParseError {
-  fn new(msg: &str) -> RablParseError {
-    RablParseError { error: msg.to_string() }
-  }
-}
-
-impl fmt::Display for RablParseError {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f,"{}",self.error)
-  }
-}
-
-impl Error for RablParseError {
-  fn description(&self) -> &str {
-    &self.error
-  }
-}
-
-// If successful, returns Some(Vec of messages)
-fn poll_messages(username: String) -> Result<Option<Vec<Message>>, Box<dyn Error>> {
-  let mut stream = establish_connection()?;
-  let payload = build_payload(vec!["poll".to_string(), username]);
-
-  stream.write(&payload)?;
-
-  // Create the buffer, currently 128 bytes
-  // we then 'read' into the buffer, filling it with whatever the server sends us
-  let mut buffer = [0 as u8; 128];
-  stream.read(&mut buffer)?;
-
-  // Now we need to take that buffer and make an vec of messages, assuming the server did not respond with nil
-  // First, check to see if buffer contains 'nil', if so simply return None
-  // Otherwise, we build the vec from the buffer contents
-  match &buffer[0..2] {
-    b"nil" => {
-      Ok(None)
-    },
-    _ => {
-      // Great! Buffer is not nil
-      let mut messages: Vec<Message> = Vec::new(); 
-
-      // Cursor - A sort of index - used to keep track of which part of the buffer we are at to aid in slicing
-      let mut cursor = 0;
-      for (i, c) in buffer.iter().enumerate() {
-
-        // If c is either the end of a source:msg block or end of the returned data
-        if c == &29 || c == &23 {
-          // We will create a new sub-slice from the beginning of our cursor to the current index
-          // This will effectively create a sub slice, of each source:msg block
-          let slice = &buffer[cursor..i].to_owned();
-
-          for (j, x) in slice.iter().enumerate() {
-            // Now we iterate the sub slice, looking for the ascii-31, which we use to seperate source from msg
-            if x == &31 {
-              // Attempt to parse the source and message from bytes to utf-8
-              match (from_utf8(&slice[0..j]), from_utf8(&slice[j+1..slice.len()])) {
-                (Ok(source), Ok(message)) => {
-                  messages.push(Message { source: source.to_string(), content: message.to_string() } )
-                },
-                _ => return Err(Box::new(RablParseError::new("Failed to parse in poll_messages")))
-              }  
-            }
-          }
-
-          // Advance our cursor to the outer-loop index+1 (Moves us to the next source:msg block)
-          cursor = i+1;
-        }
-      }
-
-      Ok(Some(messages))
-    }
-  }
 }
 
 fn poll_messages_clicked(mut cx:FunctionContext) -> JsResult<JsArray> {
