657ece837462189dcfdcbf0784249c6bd932281c
Goby <multicody10@yahoo.com>
2021-04-15 15:51:31 -0400

    uncomment purge_userdat

--- scripts/logout.js
+++ scripts/logout.js
@@ -3,7 +3,7 @@ const logoutButton = document.getElementById('logoutButton');
 logoutButton.addEventListener('click', () =>{
   let message = "There was an error trying to logout!"
   try{
-    //rabl_rust.purge_userdat();
+    rabl_rust.purge_userdat();
     location.replace("../Views/login.html")
   }catch(logout_error){
     alert(message);
