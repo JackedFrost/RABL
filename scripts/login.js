2e880c497597a6a6b01b1326a7028ad5fc658aad
klinical <zach.n500@gmail.com>
2021-05-07 00:15:16 -0500

    Removal of Rust components and prep for rebuild

--- scripts/login.js
+++ scripts/login.js
@@ -1,18 +1,4 @@
 const loginButton = document.getElementById('loginButton')
-const user = "test";
-
-  try{
-    let storedUser = rabl_rust.deserialize_login();
-    if(storedUser.Username != null || storedUser.Username != undefined){
-     let username = storedUser.Username.toString();
-     let password = storedUser.Password.toString();
-     rabl_rust.login(username, password);
-     location.replace("../Views/friends.html")
-    }
-  }catch(error){
-   console.log("No user info has been saved")
-  }
-
 
 loginButton.addEventListener('click', () => {
     let username = document.getElementById('username').value;
@@ -20,7 +6,7 @@ loginButton.addEventListener('click', () => {
     var message = "There was an error trying to login, please check your username and password, and try again"
 
     try {
-      if (rabl_rust.login(username, password)) {
+      if (false) {
         location.replace("../Views/friends.html")
       } else {
         console.log('login failed')
